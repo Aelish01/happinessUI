50f78250f07eae16ce65ebca6ba579cc0ce92a25
happinessgarden2908-jpg <happinessgarden2908@gmail.com>
2025-08-18 12:43:44 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,366 @@
+window.productsData = [
+  {
+    id: 1,
+    english: 'Mango',
+    price: 200,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAc-1hq8hyZc2K_VP8qutknfXcBrbLk8hTjA&s',
+    advantages: [
+      { key: 'ફળોના રાજા તરીકે ઓળખાતું પૌષ્ટિક ફળ છે.' },
+      { key: 'ઘરના વાતાવરણમાં સુગંધ અને હરિયાળી લાવે છે.' },
+      { key: 'ગરમ હવામાનમાં સહેલાઈથી ઉગે છે.' },
+    ],
+  },
+  {
+    id: 2,
+    english: 'Banana',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZecTDdd9vXlxn2EXccRaapnCnqfwOXyKjKP1Og1_x996m1icD8PUOpJEx3Ei9ZCD5dNw&usqp=CAU',
+  },
+  {
+    id: 3,
+    english: 'Pomegranate',
+    price: 150,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGTQb1oJuhUIUdV1zmXwGbR5AISUJUFlRFXw&s',
+  },
+  {
+    id: 4,
+    english: 'Guava',
+    price: 120,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvTpp9K4dyzU8VhtOXRlEgdF4nmLxlFAl-PA&s',
+  },
+  {
+    id: 5,
+    english: 'Lemon',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://images-eu.ssl-images-amazon.com/images/I/71WzNz3lPRL._AC_UL495_SR435,495_.jpg',
+  },
+  {
+    id: 6,
+    english: 'Chikoo (Sapota)',
+    price: 200,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo55-X2W7JseTJAEUhVZkihHEYMMbVgdCcog&s',
+  },
+  {
+    id: 7,
+    english: 'Jamboo',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDjv2jpj_L68dG9EL77jjwFeVnHilFluBD2Q&s',
+  },
+  {
+    id: 8,
+    english: 'Fig',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJL_8YtAZE230YfAmBYaom2ipZilyut9_Klg&s',
+  },
+  {
+    id: 9,
+    english: 'Jackfruit (Dwarf)',
+    price: 200,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-4aMq6FgazWxvJ5mwg-wiawW9ONBM93ZKzQ&s',
+  },
+  {
+    id: 10,
+    english: 'Papaya',
+    price: 70,
+    category: 'fruit',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2022/7/MV/IR/JM/106091433/fresh-papaya-1000x1000.jpg',
+  },
+  {
+    id: 11,
+    english: 'Lychee',
+    price: 300,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVT2psPBetw6PVglgGXUil_uFfP1dKbvaOzw&se',
+  },
+  {
+    id: 12,
+    english: 'Mosambi',
+    price: 200,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfqumd-r-Ppd_IO2k_Hw-ntCo7nXxQ4IyQgA&s',
+  },
+  {
+    id: 13,
+    english: 'Mulberry',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://magazine.outdoornebraska.gov/wp-content/uploads/2024/08/mulberries.png',
+  },
+  {
+    id: 14,
+    english: 'Coconut',
+    price: 250,
+    category: 'fruit',
+    image:
+      'https://www.mtechgardens.com/cdn/shop/files/Coconut-Kalpasree-Hybrid_c9c663b4-e8b3-4edc-954d-674c2d9d62d3.png?crop=center&height=800&v=1717143280&width=800',
+  },
+  {
+    id: 15,
+    english: 'Lemon Khagdi',
+    price: 150,
+    category: 'fruit',
+    image:
+      'https://5.imimg.com/data5/XE/YV/MY-16365438/kagzi-lemon-plant-1000x1000.jpg',
+  },
+  {
+    id: 16,
+    english: 'All Time Mango',
+    price: 300,
+    category: 'fruit',
+    image:
+      'https://m.media-amazon.com/images/I/6180HfYgZnL._UF1000,1000_QL80_.jpg',
+  },
+  {
+    id: 17,
+    english: 'Ramfal',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSigCcekg4Y211o8jjn-ILYImNpQlYn2S66NH8cbCnYnofwUJ0kCs8Xb4vhiobOCFeOaFU&usqp=CAU',
+  },
+  {
+    id: 18,
+    english: 'Gooseberry ',
+    price: 80,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJJZWc4gJV2AsSBqB4jlUoR096a22UT68ykQ&s',
+  },
+  {
+    id: 19,
+    english: 'Star Gooseberry',
+    price: 80,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoSBPgYW5mfbDCMSIPs_pTfSC3lQAhOwEg-g&s',
+  },
+  {
+    id: 20,
+    english: 'Almond (Badam)',
+    price: 80,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW7bVj3xHe3Pu4ElsVotEvaZLam4Bb_FyJxA&s',
+  },
+  {
+    id: 21,
+    english: 'Lady Palm',
+    price: 360,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRELK6FG0nhMFqdlsUtf62yU07gH_n9y-pqqA&s',
+  },
+  {
+    id: 22,
+    english: 'Cherry',
+    price: 100,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE-MFPAmhwNcuMghSqgi8WMKWw8VMS43rIGQ&s',
+  },
+  {
+    id: 23,
+    english: 'Desi Rose',
+    price: 50,
+    category: 'decorative',
+    image:
+      'https://5.imimg.com/data5/ECOM/Default/2024/2/389923283/AL/YF/YB/133331885/lightpinknadanroseliveplant-seed2plant-1-500x500.png',
+  },
+  {
+    id: 24,
+    english: 'Bilipatr',
+    price: 80,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIsyrt5G8oSKMB6ltdYBbNKPMV9Oy_K7vpTw&s',
+  },
+  {
+    id: 25,
+    english: 'Star Fruit',
+    price: 250,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmUwwbgg4qyCyaz-AJwv8Q1vCJ45MouYpLpA&s',
+  },
+  {
+    id: 26,
+    english: 'Orange',
+    price: 200,
+    category: 'fruit',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfj8FzwWjGqhH_9cCWSIlK3acBrSsCF_sQZA&s',
+  },
+  {
+    id: 27,
+    english: 'Neem (Limdo)',
+    price: 80,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYD5nlh33Cm4OZ2Mhacql5FWdpoSTq4U6aZQ&s',
+  },
+  {
+    id: 28,
+    english: 'Champa',
+    price: 100,
+    category: 'decorative',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5FSvhqFC1N6uY4P4LtKFsysgy98kZ20cc4w&s',
+  },
+  {
+    id: 29,
+    english: 'Adenium',
+    price: 150,
+    category: 'decorative',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNcPbZAmE5C82qW6thBfNz_tSWBtDMlyl5wA&s',
+  },
+  {
+    id: 30,
+    english: 'Tej Patta',
+    price: 50,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgvGtllmHzFnnZiY0JtaTeEhFfNX1SmrijtQ&s',
+  },
+  {
+    id: 31,
+    english: 'Areca Palm',
+    price: 250,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBo5N1gxtDCbHD6X5_b8XvuPGAsUsEQmxuFQ&s',
+  },
+  {
+    id: 32,
+    english: 'Fern',
+    price: 50,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLiubzo5rvuMcc1oAWbF_fH2tQzK4atvGvXg&s',
+  },
+  {
+    id: 33,
+    english: 'Travellers Palm',
+    price: 150,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpkiOSu23JHi4X4CSF7hK7xyDIX8uYyv9W6A&s',
+  },
+  {
+    id: 34,
+    english: 'Morpankhi',
+    price: 100,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbhYoVKRcZVZsaxWqWKprYzfL0yugWlkmTBA&s',
+  },
+  {
+    id: 35,
+    english: 'Tarmanlia',
+    price: 150,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPJLNG15Sh5bwHIbc14_sBlIYxxzmIs5FBww&s',
+  },
+  {
+    id: 36,
+    english: 'Snake Plant',
+    price: 150,
+    category: 'indoor',
+    image:
+      'https://png.pngtree.com/png-clipart/20220707/ourmid/pngtree-realistic-snake-plant-in-hand-drawn-style-png-image_5744918.png',
+  },
+  {
+    id: 37,
+    english: 'Dieffenbachia',
+    price: 130,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Zl0i5MtJymlGjeJZo19uWZQqdP0JCL6T5OiHWqkxeHUHwnsiLvAWcCIYG1qPvWaK-k4&usqp=CAU',
+  },
+  {
+    id: 38,
+    english: 'Dracaena Mix',
+    price: 140,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2XpaWH69B2HmWiheMWQSlBEe_cRLma3-gAyFzc6dv6b3CPHgBjSHr2NnMliugHQD2RZk&usqp=CAU',
+  },
+  {
+    id: 39,
+    english: 'Syngonium Mix',
+    price: 80,
+    category: 'indoor',
+    image:
+      'https://rootsraja.in/cdn/shop/files/Syngonium_Gold_Allusion.png?v=1750321651',
+  },
+  {
+    id: 40,
+    english: 'Yellow Dracaena',
+    price: 100,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvDa9SwCC8kVx8eFuvmzHsW2gVxLBcKF9MAXg2BTVKb5nNdqNSloVnCLCYQXXETgfUpj0&usqp=CAU',
+  },
+  {
+    id: 41,
+    english: 'Green Bamboo',
+    price: 80,
+    category: 'indoor',
+    image: 'https://m.media-amazon.com/images/I/71jYS4temqL.jpg',
+  },
+  {
+    id: 42,
+    english: 'Ribbon Grass',
+    price: 80,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW9GaCDD7n1wDeB9k2i1K1vCF984WJ362aIdDPr3Wmkrrsg8bEATganCcL8OW1CbgWhos&usqp=CAU',
+  },
+  {
+    id: 43,
+    english: 'Lemon Grass',
+    price: 50,
+    category: 'outdoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6K1Hw7Krd_dIZwH_9pJNytHyh3JF8VaKEOw&s',
+  },
+  {
+    id: 44,
+    english: 'Christmas Tree',
+    price: 150,
+    category: 'decorative',
+    image:
+      'https://png.pngtree.com/png-clipart/20230930/original/pngtree-christmas-tree-in-pot-png-image_13022856.png',
+  },
+  {
+    id: 45,
+    english: 'Jade Plant',
+    price: 130,
+    category: 'indoor',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQpiwvmFPDB3bqN9ThKdYFmdIgwHFBsgKIOQ&s',
+  },
+]
